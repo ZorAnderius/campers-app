8ab38c3ecb48f1867a13c724f9f90d09380d3092
ZorAnderius <gabriel.zorander@gmail.com>
2024-11-27 21:25:56 +0000

    add header observer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,21 @@
+import { useRef, useState } from 'react';
 import { Container } from '../Container/Container';
+import { HeaderObserver } from '../HeaderObserver/HeaderObserver';
 import { Navigation } from '../Navigation/Navigation';
 import styles from './Header.module.css';
+import clsx from 'clsx';
 
 export const Header = () => {
+  const [isVisible, setIsVisible] = useState();
+  const observRef = useRef();
   return (
-    <header className={styles.header}>
-      <Container>
-        <Navigation />
-      </Container>
-    </header>
+    <>
+      <HeaderObserver ref={observRef} setVisible={setIsVisible} />
+      <header className={clsx(styles['header'], !isVisible && styles['fixed'])}>
+        <Container>
+          <Navigation />
+        </Container>
+      </header>
+    </>
   );
 };
---
+++ src/components/HeaderObserver/HeaderObserver.jsx
@@ -0,0 +1,30 @@
+import { forwardRef, useEffect, useMemo } from 'react';
+import styles from './HeaderObserver.module.css';
+
+export const HeaderObserver = forwardRef(({ setVisible }, ref) => {
+  const handleEntries = entries => {
+    const [entry] = entries;
+    setVisible(entry.isIntersecting);
+  };
+
+  const option = useMemo(() => {
+    return {
+      root: null,
+      rootMargin: '0px 0px 0px 0px',
+      threshold: 1,
+    };
+  }, []);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleEntries, option);
+    const currentTarget = ref.current;
+    currentTarget && observer.observe(currentTarget);
+
+    return () => {
+      currentTarget && observer.unobserve(currentTarget);
+    };
+  }, [option, ref]);
+  return <div ref={ref} className={styles['header-observe']}></div>;
+});
+
+HeaderObserver.displayName = 'HeaderObserver';
