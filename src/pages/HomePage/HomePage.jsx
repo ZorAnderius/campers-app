6fd29160805c360dea9ba84adaf81d8196e43744
ZorAnderius <gabriel.zorander@gmail.com>
2024-11-29 14:32:47 +0000

    fix dublicate issues when navigate to catalog page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
 import { Container } from '../../components/assets/Container/Container';
 import { NavButton } from '../../components/assets/NavButton/NavButton';
 import { ROUTE } from '../../constants/constants';
 import styles from './HomePage.module.css';
+import { useDispatch } from 'react-redux';
+import { resetVehicals } from '../../redux/vehicles/slice';
+import { resetFilter } from '../../redux/filters/slice';
 
 const HomePage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(resetVehicals());
+    dispatch(resetFilter());
+  }, [dispatch]);
+
   return (
     <main className={styles['main']}>
       <section className={styles['hero']}>
