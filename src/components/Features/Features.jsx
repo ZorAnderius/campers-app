a54eb1f16b1e42c2fdc028f04dff69acf17b12fc
ZorAnderius <gabriel.zorander@gmail.com>
2024-11-30 17:42:31 +0000

    add features for single camper page

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,5 +1,41 @@
+import { useSelector } from 'react-redux';
+import { selectCurrentCamper } from '../../redux/vehicles/selector';
+import {
+  serializeFacilitiesBadge,
+  serializeVehicleDetails,
+} from '../../helpers/camperCard/serializeCamperData';
+import { CamperFacilities } from '../CamperFacilities/CamperFacilities';
+import Title from '../assets/Title/Title';
 import styles from './Features.module.css';
 
 export const Features = () => {
-  return <h1 className={styles}>features</h1>;
+  const camper = useSelector(selectCurrentCamper);
+  const facilities = serializeFacilitiesBadge(camper);
+  const camperDetails = serializeVehicleDetails(camper);
+  return (
+    <div className={styles['feature-container']}>
+      {facilities?.length > 0 && (
+        <ul className={styles['feature-facilities']}>
+          {facilities?.map(facility => (
+            <li key={facility.id} className={styles['facility-item']}>
+              <CamperFacilities info={facility} />
+            </li>
+          ))}
+        </ul>
+      )}
+      <div className={styles['camper-details']}>
+        <Title>Vehicle details</Title>
+        {camperDetails?.length > 0 && (
+          <ul className={styles['details-list']}>
+            {camperDetails?.map(({ feature, title, id }) => (
+              <li key={id} className={styles['details-item']}>
+                <p>{title}</p>
+                <p>{feature}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 };
